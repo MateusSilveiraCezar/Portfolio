19646580053cba66dd32cc640b3917e86cea6f2d
MateusSilveiraCezar <mateussilveiracezar08@gmail.com>
2025-08-14 18:18:45 -0300

    Finalizando o portfolio

--- meu-portfolio/src/components/Header.jsx
+++ meu-portfolio/src/components/Header.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import styles from "../styles/Header.module.css";
 import foto from "../assets/mateus.jpg";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <img src={foto} alt="Logo" className={styles.logo} />
       <span className={styles.name}>MATEUS SILVEIRA CEZAR</span>
-      <nav className={styles.nav}>
+
+      {/* Ícone do menu hambúrguer */}
+      <div
+        className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+
+      <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
         <ul className={styles.menu}>
           <li><a href="/#home">HOME</a></li>
           <li><a href="/#sobre">SOBRE</a></li>
